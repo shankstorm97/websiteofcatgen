e52366a3c9519372b6efd120d1b6304c1dfd782d
shankstorm97 <work.prabhdeepsinngh@gmail.com>
2022-01-31 16:37:36 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function catGenerator() {
+    var image = document.createElement('img');
+    var div = document.getElementById('cat-gen');
+    image.src = "/project_cat_2/cat.jpeg";
+    div.appendChild(image);
+}
